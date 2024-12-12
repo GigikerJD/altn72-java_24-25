0b80cfcc865237abe6e7985def17ada5564bc966
GigikerJD <nelgie.ferrol94@gmail.com>
2024-12-12 13:40:26 +0100

    projet - init

---
+++ src/test/java/com/projet/altn72/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.altn72;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
