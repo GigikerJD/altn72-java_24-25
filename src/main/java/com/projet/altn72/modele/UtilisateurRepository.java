0856ef716a4d9d9472f6578f9e8d8d55ee1948e6
GigikerJD <nelgie.ferrol94@gmail.com>
2024-12-14 11:45:02 +0100

    correction users repo

--- src/main/java/com/projet/altn72/modele/UtilisateurRepository.java
+++ src/main/java/com/projet/altn72/modele/UtilisateurRepository.java
@@ -3,7 +3,8 @@ package com.projet.altn72.modele;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.projet.altn72.entite.UtilisateurEntite;
+import com.projet.altn72.modele.composite.Utilisateur;
 
-public interface UtilisateurRepository extends JpaRepository<UtilisateurEntite, String> {
+public interface UtilisateurRepository extends JpaRepository<UtilisateurEntite, Utilisateur> {
 
 }
