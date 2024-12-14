8cbd8fad0b229dc5a729bf4dfcf8f87ca58828d0
GigikerJD <nelgie.ferrol94@gmail.com>
2024-12-14 11:45:22 +0100

    ajout méthodes service

--- src/main/java/com/projet/altn72/service/ConsulteService.java
+++ src/main/java/com/projet/altn72/service/ConsulteService.java
@@ -1,8 +1,12 @@
 package com.projet.altn72.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.projet.altn72.entite.ConsulteEntite;
+import com.projet.altn72.entite.UtilisateurEntite;
 import com.projet.altn72.modele.ConsulteRepository;
 
 @Service
@@ -10,4 +14,18 @@ public class ConsulteService {
 
     @Autowired
     private ConsulteRepository consulteRepository;
+
+
+    public List<ConsulteEntite> getConsults(){
+        return consulteRepository.findAll();
+    }
+
+    public List<UtilisateurEntite> getUtilisateursParOutil(String titre){
+        return consulteRepository
+            .findAll()
+            .stream()
+            .filter(c -> c.getOutil().getTitre().equals(titre))
+            .map(c -> c.getUtilisateur())
+            .toList();
+    }
 }
--- src/main/java/com/projet/altn72/service/UtilisateurService.java
+++ src/main/java/com/projet/altn72/service/UtilisateurService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.projet.altn72.entite.UtilisateurEntite;
 import com.projet.altn72.modele.UtilisateurRepository;
+import com.projet.altn72.modele.composite.Utilisateur;
 
 @Service
 public class UtilisateurService {
@@ -14,21 +15,16 @@ public class UtilisateurService {
     @Autowired
     private UtilisateurRepository utilisateurRepository;
 
+    private Utilisateur user = null;
+
     public List<UtilisateurEntite> getUtilisateurs(){
         return utilisateurRepository.findAll();
     }
 
-    public UtilisateurEntite getUtilisateurParEmail(String email){
-        return utilisateurRepository.findById(email).get();
-    }
-
-    public void creerUtilisateur(UtilisateurEntite utilisateurEntite) {
-        UtilisateurEntite copy = utilisateurEntite;
-    }
-
-    public UtilisateurEntite getUtilisateurParPseudo(String pseudo) {
-        return utilisateurRepository.findById(pseudo).get();
-    }
+    public UtilisateurEntite getUtilisateurParEmail(String email, String pseudo){
+        user = new Utilisateur(email, pseudo);
+        return utilisateurRepository.findById(user).get();
+    }  
 
     
 }
