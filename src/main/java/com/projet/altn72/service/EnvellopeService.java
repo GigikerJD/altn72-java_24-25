886f8481653e055769ff1109cbf77b831e0f016c
GigikerJD <nelgie.ferrol94@gmail.com>
2024-12-15 20:15:29 +0100

    ajout méthodes pour service

--- src/main/java/com/projet/altn72/service/EnvellopeService.java
+++ src/main/java/com/projet/altn72/service/EnvellopeService.java
@@ -1,8 +1,11 @@
 package com.projet.altn72.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.projet.altn72.entite.EnveloppeEntite;
 import com.projet.altn72.modele.EnveloppeRepository;
 
 @Service
@@ -10,4 +13,12 @@ public class EnvellopeService {
 
     @Autowired
     private EnveloppeRepository enveloppeRepository;
+
+    public List<EnveloppeEntite> getTousLesFeedbacksParOutil(String titre){
+        return enveloppeRepository
+            .findAll()
+            .stream()
+            .filter(env -> env.getTitre().equals(titre))
+            .toList();
+    }
 }
--- src/main/java/com/projet/altn72/service/UtilisateurService.java
+++ src/main/java/com/projet/altn72/service/UtilisateurService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 
 import com.projet.altn72.entite.UtilisateurEntite;
 import com.projet.altn72.modele.UtilisateurRepository;
@@ -21,10 +22,29 @@ public class UtilisateurService {
         return utilisateurRepository.findAll();
     }
 
-    public UtilisateurEntite getUtilisateurParEmail(String email, String pseudo){
-        user = new Utilisateur(email, pseudo);
+    public UtilisateurEntite getUtilisateurParEmail(String email){
+        user = new Utilisateur(email);
         return utilisateurRepository.findById(user).get();
+    }
+
+    public void creerNouvelUtilisateur(UtilisateurEntite newUser) {
+        user = new Utilisateur(newUser.getEmail());
+        boolean isAlreadyEnlisted = utilisateurRepository.findById(user).isPresent();
+        if(isAlreadyEnlisted){
+            System.err.println("Utilisateur déjà enregistré !");
+        }else{
+            utilisateurRepository.save(newUser);
+            System.out.println("Vous êtes enregistré !");
+        }
     }  
 
+    public void supprimerUtilisateur(UtilisateurEntite user){
+        utilisateurRepository.delete(user);
+    }
+
+    public void Login(Model model) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Login'");
+    }
     
 }
